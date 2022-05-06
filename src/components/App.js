28dc2af881cef633c5e48ad4946237f8f62cd7be
hmcvay <hayleymcvay8@gmail.com>
2022-05-06 10:34:32 -0700

    WIP: styling Home

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View,
+} from 'react-native';
 import Home from './Home';
 
 import { NavigationContainer } from '@react-navigation/native';
@@ -10,21 +14,35 @@ const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-   <NavigationContainer>
-     <Stack.Navigator>
-       <Stack.Screen name="Home" component={Home} />
-     </Stack.Navigator>
-   </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Home'
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#2980ff',
+          },
+          headerTintColor: '#d2e6ef',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
+        <Stack.Screen 
+          name="Home" 
+          component={Home}
+          // options={{headerShown: false}} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,24 @@ import React from 'react';
 import {
   View,
   Text,
+  StyleSheet,
 } from 'react-native';
 
 function Home({navigation}){
   return(
-    <View>
+    <View style={styles.container}>
       <Text>This is a home screen</Text>
     </View>
   )
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default Home;
