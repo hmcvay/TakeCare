e913c89d5db347fa5a9326adfeacccedc5fefa4e
hmcvay <hayleymcvay8@gmail.com>
2022-05-06 10:20:39 -0700

    Add React Native navigation to App, and edit entry point in index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import { registerRootComponent } from 'expo';
 
-import App from './App';
+
+
+import App from './src/components/App';
+
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
