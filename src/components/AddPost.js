0cba82ae897cc246918e406c808614b83069c195
hmcvay <hayleymcvay8@gmail.com>
2022-05-13 11:23:21 -0700

    Complete AddPost component

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
@@ -6,10 +6,13 @@ import {
   SafeAreaView,
   TouchableOpacity,
 } from 'react-native';
-import {db} from './../firebase/fireabase-config';
-import {v4 as uuid} from 'uuid';
+import { db } from './../firebase/firebase-config';
+import { v4 as uuid } from 'uuid';
 import PropTypes from 'prop-types';
-import {doc, setDoc, getDocs, Timestamp} from 'firebase.firestore';
+import { doc, setDoc, getDocs, Timestamp } from 'firebase/firestore';
+import { TextInput } from 'react-native-web';
+import { CheckBox } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
 
 
 const AddPost = ({navigation}) => {
@@ -18,6 +21,9 @@ const AddPost = ({navigation}) => {
   const onChangeTitle = titleValue => setTitle(titleValue);
   const [descriptionInput, setDescription] = useState('');
   const onChangeDescription = descriptionValue => setDescription(descriptionValue);
+  const [locationInput, setLocation] = useState('');
+  const onChangeLocation = locationValue => setLocation(locationValue);
+  const [isUrgentBox, setIsUrgentBox] = useState(false);
 
 
   const onPressAddPost = async () => {
@@ -27,39 +33,103 @@ const AddPost = ({navigation}) => {
         id: id,
         title: titleInput,
         description: descriptionInput,
+        location: locationInput,
         time: Timestamp.fromDate(newDate()),
+        is_urgent: isUrgentBox,
         is_met: false,
       });
+      navigation.navigate('PostList', {updated: true});
+    } else {
+      alert('Please add a Title');
     }
 
-  }
-
-
+  };
   return(
-    <SafeAreaView
+    <View
       style={{
         flex: 1,
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#efeee9',
       }}>
-      <TouchableOpacity
-        onPress={() => navigation.navigate('PostList')}
-        style={{
-          width: '80%',
-          padding: 15,
-          color: '#fcc9a8',
-          borderRadius: 10,
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          marginBottom: 20,
-        }}>
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <Text>ADD POST FORM COMING</Text>
+      <View>
+        <View>
+          <TextInput 
+            placeholder="Title"
+            onChangeText={onChangeTitle}
+            maxLength={50}
+            autoCapitalize="words"
+          />
+          <TextInput 
+            placeholder="Description"
+            onChangeText={onChangeDescription}
+            maxLength={150}
+            autoCapitalize="sentences"
+          />
+          <TextInput 
+            placeholder="Portland, Oregon"
+            onChangeText={onChangeLocation}
+            maxLength={30}
+            autoCapitalize="words"
+          />
+        </View>
+        <View 
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}>
+          <CheckBox
+            center
+            checkedIcon={
+              <Icon 
+                name="exclamation"
+                type="material"
+                color="#FFFFFF"
+                size={25}
+                iconStyle={{marginRight: 10}}
+              />
+            }
+            uncheckedIcon={
+              <Icon 
+                name="exclamation"
+                type="material"
+                color="#D3D3D3"
+                size={25}
+                iconStyle={{marginRight: 10}}
+              />
+            }
+            checked={isUrgentBox}
+            onPress={() => setIsUrgentBox(!isUrgentBox)}
+          />
+          <Text style={{fontSize: 16, width: windowWidth -90, marginTop: 20}}>
+            {isUrgentBox
+              ? "This ask is urgent!"
+              : "This ask is not urgent"}
+          </Text>
+        </View>
+      </View>
+      <TouchableOpacity>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginBottom: 35,
+            marginLeft: 50,
+            marginRight: 50,
+            backgroundColor: "#efeee9",
+          }}>
+          <Icon 
+            name="plus"
+            size={35}
+            color="#000000"
+            onPress={() => onPressAddPost()}
+          />
         </View>
       </TouchableOpacity>
-    </SafeAreaView> 
-  )
+    </View>
+  );
 };
 
 export default AddPost;
