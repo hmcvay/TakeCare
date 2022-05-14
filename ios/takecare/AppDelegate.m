606ba84e2ff317fbcee27330f99f512ecb91e7bd
hmcvay <hayleymcvay8@gmail.com>
2022-05-13 17:07:09 -0700

    update config and packages

--- ios/takecare/AppDelegate.m
+++ ios/takecare/AppDelegate.m
@@ -29,6 +29,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  // [GMSServices provideAPIKey:@"REACT_APP_FIREBASE_API_KEY"];
 #if defined(FB_SONARKIT_ENABLED) && __has_include(<FlipperKit/FlipperClient.h>)
   InitializeFlipper(application);
 #endif
